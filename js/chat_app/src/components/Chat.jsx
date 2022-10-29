838e2507edb956f930d113c40378a225765263ed
Nikita <nikycherenko@gmail.com>
2022-10-29 22:22:55 +0300

    implement_chat_window v1

--- js/chat_app/src/components/Chat.jsx
+++ js/chat_app/src/components/Chat.jsx
@@ -5,15 +5,17 @@ import { useSelector } from 'react-redux';
 import { Header } from './Header';
 import { UserModal } from '../modals/userForm';
 import { ChatModal } from '../modals/ChatForm';
+import ChatWindow from './ChatWindow/ChatWindow';
 
 function Chat() {
   const isAdmin = useSelector((state) => state.auth.auth.isAdmin);
 
   return (
-    <Box sx={{
-      width: '100vw',
-      height: '100vh'
-    }}
+    <Box
+      sx={{
+        width: '100vw',
+        height: '100vh'
+      }}
     >
       <Container fixed>
         <Header />
@@ -23,26 +25,26 @@ function Chat() {
             height: '85vh'
           }}
         >
-          <Box sx={{
-            width: '400px',
-            borderRight: '1px solid',
-            pr: '10px',
-            height: '100%'
-          }}
+          <Box
+            sx={{
+              width: '400px',
+              borderRight: '1px solid',
+              pr: '10px',
+              height: '100%'
+            }}
           >
-            {isAdmin
-              ? <UserModal />
-              : null}
+            {isAdmin ? <UserModal /> : null}
             <ChatModal />
           </Box>
-          <Box sx={{
-            width: '100%',
-            borderRight: '1px solid',
-            p: '0 10px',
-            height: '100%'
-          }}
+          <Box
+            sx={{
+              width: '100%',
+              borderRight: '1px solid',
+              p: '0 10px',
+              height: '100%'
+            }}
           >
-            ChatWindow
+            <ChatWindow />
           </Box>
         </Box>
       </Container>
--- js/chat_app/src/services/chatServices.js
+++ js/chat_app/src/services/chatServices.js
@@ -34,5 +34,41 @@ export const chatServices = {
     } catch (error) {
       console.log(error);
     }
+  },
+  getChatById: async (chatId, authToken) => {
+    try {
+      const data = await fetch(`/api/chats/${chatId}`, {
+        method: 'GET',
+        headers: {
+          'content-type': 'application/json',
+          'Auth-Token': authToken
+        }
+      });
+
+      const response = await data.json();
+
+      return response;
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  sendMessageByChatId: async (chatId, authToken, message, authorId) => {
+    try {
+      // eslint-disable-next-line no-debugger
+      debugger;
+      await fetch(`/api/chats/${chatId}/messages`, {
+        method: 'POST',
+        headers: {
+          'content-type': 'application/json',
+          'Auth-Token': authToken
+        },
+        body: {
+          authorId,
+          message
+        }
+      });
+    } catch (error) {
+      console.log(error);
+    }
   }
 };
